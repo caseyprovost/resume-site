9a8660abc861df06ee5f0df1b3a01bad6c9c3e39
Casey Provost <caseyr.provost@gmail.com>
2019-08-13 11:09:07 -0400

    fix links and site title

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,10 @@
         Howdy!
       </h2>
       <p class="text-xl sm:text-2xl md:text-3xl lg:text-4xl md:font-light text-gray-700 w-11/12 mt-6">
-        My name is Casey Provost, and I craft delightful web applications and APIs with <a href="#" class="border-b border-gray-900 pb-px md:pb-1 text-gray-900">Ruby on Rails</a>.
+        My name is Casey Provost, and I craft delightful web applications and APIs with <a href="https://rubyonrails.org/" target="_new" class="border-b border-gray-900 pb-px md:pb-1 text-gray-900">Ruby on Rails</a>.
       </p>
       <p class="text-xl sm:text-2xl md:text-3xl lg:text-4xl md:font-light text-gray-700 w-11/12 mt-8">
-        When I am not building stuff I am writing about it on my engineering <a href="https://caseyprovost.com" class="border-b border-gray-900 pb-px md:pb-1 text-gray-900">blog</a>. I write about APIs, design patterns, and how to build great stuff together.
+        When I am not building stuff I am writing about it on my engineering <a href="https://caseyprovost.com" target="_new" class="border-b border-gray-900 pb-px md:pb-1 text-gray-900">blog</a>. I write about APIs, design patterns, and how to build great stuff together.
       </p>
       <p class="text-xl sm:text-2xl md:text-3xl lg:text-4xl md:font-light text-gray-700 w-11/12 mt-8">
         I am currently accepting new clients. Please <a class="border-b border-gray-900 pb-px md:pb-1 text-gray-900" href="mailto:caseyr.provost@gmail.com">reach out</a> if you would like to explore working together.
