0eb177589dee5681f90b4fc113e9e68146b9b95e
Casey Provost <caseyr.provost@gmail.com>
2019-08-13 10:59:32 -0400

    rework icons

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -13,13 +13,13 @@
     </div>
     <div class="w-1/5 text-right">
       <div class="text-gray-500 text-2xl pr-8 py-4">
-        <a href="https://www.linkedin.com/in/caseyprovost/" target="_new" class="mx-1 hover:text-blue-700">
+        <a href="https://www.linkedin.com/in/caseyprovost/" target="_new" class="mx-1 hover:text-blue-700  inline-block" style="width: 23px">
           <font-awesome-icon :icon="['fab', 'linkedin']" />
         </a>
-        <a href="https://github.com/caseyprovost" target="_new" class="mx-1 hover:text-blue-700">
+        <a href="https://github.com/caseyprovost" target="_new" class="mx-1 hover:text-blue-700  inline-block" style="width: 23px">
           <font-awesome-icon :icon="['fab', 'github']" />
         </a>
-        <a href="https://twitter.com/caseyprovost" target="_new" class="mx-1 hover:text-blue-700">
+        <a href="https://twitter.com/caseyprovost" target="_new" class="mx-1 hover:text-blue-700  inline-block" style="width: 23px">
           <font-awesome-icon :icon="['fab', 'twitter']" />
         </a>
       </div>
